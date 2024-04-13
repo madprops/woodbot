c23b0deb205cffda8c34530b16ca939b67eb760f
Auric Vente <mprops@protonmail.com>
2024-04-13 14:43:31 -0600

    Mods

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def on_message(ws, message):
         argument = " ".join(args).strip()
         room_id = data["roomId"]
 
-        if cmd in ["!ai", "!a", "!i", ".ai", ";ai", ",ai"]:
+        if cmd in ["!ai", "!a", "!i", ".ai", ";ai", ",ai", "woody,"]:
             respond(ws, room_id, argument, uname)
 
 
